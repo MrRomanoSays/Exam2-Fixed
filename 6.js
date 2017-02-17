ac90052607de34ac7e7c943daa8576aa3e8a0974
Lawrence Romano <l.j.romano@gmail.com>
2017-02-17 13:49:55 -0500

    Uploading latest version of 6.js

--- 6.js
+++ 6.js
@@ -1,10 +1,19 @@
+//COME BACK TO THIS...
+
 const data = require('./data.js')
+const { map, filter, reduce, compose, take, contains, propEq, propSatisfies } = require('ramda')
 
 // Is there a chance of rain this weekend?  return either yes or no.
 
 //Expected Output:
 // Is there a chance of rain this weekend? Yes
 
-var result = // your code gooes here
+
+// your code gooes here
+
+var result = compose (
+
+)(data.hourly_forecast)
+
 
 console.log("Is there a chance of rain this weekend?", result)
